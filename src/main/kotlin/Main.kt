20a9c341084ce931216a8901119386f38b0cbaaf
Jorjai <jorge.are.fdz@gmail.com>
2024-12-21 01:34:10 +0100

    Change the read file to the one passed as argument

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,14 @@ package net.jorjai
 import java.io.File
 
 fun main(args: Array<String>) {
-    // Open the file specified as argument 1
+    try {
+        // Open the file specified as argument 1
+        var file = File(args[0])
+        // Execute the interpreter
+        var interpreter = Interpreter(file)
+        interpreter.execute()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 
-
-    var file = File("D:\\Repositorios\\SimpleCodeInterpreter\\src\\test\\resources\\input.txt")
-
-    // Execute the interpreter
-    var interpreter = Interpreter(file)
-    interpreter.execute()
 }
